12529a5e217f33a6c7ec80ad0ccb5fc681d379f3
Иван Сабиров <sabirivan@yandex.com>
2023-12-03 15:53:31 +0600

    Update files

--- src/actions/messages.py
+++ src/actions/messages.py
@@ -1,18 +1,18 @@
-from gtts import gTTS
-from aiogram import Bot, Dispatcher, types
-from config.telegram import TELEGRAM_BOT_TOKEN
+from gtts import gTTS                               # Importing a text-to-speech module
+from aiogram import Bot, Dispatcher, types          # Importing Box, Dispatcher and types from aiogram
+from config.telegram import TELEGRAM_BOT_TOKEN      # Importing a Telegram token from the Telegram configurator
 
-bot = Bot(token = TELEGRAM_BOT_TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token = TELEGRAM_BOT_TOKEN)               # Connecting the application to the Telegram bot
+dp = Dispatcher(bot)                                # Connecting to the Telegram bot dispatcher application
 
 @dp.message_handler(commands = ["start", "help"])
-async def send_welcome(message: types.Message):
+async def send_welcome(message: types.Message):     # Using the function when entering the start command
     await message.reply("Привет! Я бот-дублер! Введите что-нибудь!")
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    tts = gTTS(message.text, lang = "ru")
+async def echo(message: types.Message):             # Using the function to vocalize the entered text
+    tts = gTTS(message.text, lang = "ru")           # Receiving and voicing text in Russian
 
-    tts.save(f"{message.from_user.id}.mp3")
+    tts.save(f"{message.from_user.id}.mp3")         # Saving an audio recording
     
     await message.answer_voice(open(f"{message.from_user.id}.mp3", "rb"))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from aiogram import executor
-from actions.messages import dp
+from aiogram import executor    # importing the executor module from the aiogram module
+from actions.messages import dp # importing the bot dispatcher
 
+# starting the bot
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates = True)
